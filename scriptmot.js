ef1793efdfa46a86122d6a556408fd506492bcf7
Adem Ben Zineb <117071852+Adembenzineb@users.noreply.github.com>
2025-03-24 11:29:42 +0100

    Update scriptmot.js

--- scriptmot.js
+++ scriptmot.js
@@ -1,19 +1,29 @@
 function GeneratePassword() {
+
+    //Set password length range
     let xmin = 8;
     let xmax = 15;
+
+    //Generate a length randomly
     let x = Math.random() * (xmax - xmin + 1) + xmin;
 
-    let min = 33;
-    let max = 126;
+
+    //Creating the password randomly using printable ASCII characters
+    const min = 33;
+    const max = 126;
     let ch = ""
     for (let i = 0; i < x; i++) {
         let y = Math.random() * (max - min + 1) + min;
         ch += String.fromCharCode(y)
     }
+    //Display the generated password
     document.getElementById("zone").value = ch;
-    console.log(ch)
+
+    
+    //console.log(ch)
 }
 
+//Copying the password 
 function copy(){
     let password = document.getElementById("zone");
     password.select();
